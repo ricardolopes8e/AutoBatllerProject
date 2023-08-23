13b2bcd026d265cdf8a07e77f0c1218bcad0a5c5
ricardolopes8e <ricardolopes8e@gmail.com>
2023-08-23 22:38:20 +0100

    level transitions

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogError(Input.touchCount);
+        if(Input.touchCount > 0)
+        {
+            LoadNextLevel();
+        }
+
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
